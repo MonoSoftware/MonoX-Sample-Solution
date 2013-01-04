081fa46456779b11c68c6324752e9896ad6ce5ff
khorvat <khorvat@icodeteam.net>
2013-01-04 21:07:22 +0100

    Added Popular Group WebPart

---
+++ Code/Layers/Repositories/PopularGroupRepository.cs
@@ -0,0 +1,118 @@
+﻿using MonoSoftware.Core;
+using MonoSoftware.MonoX.DAL;
+using MonoSoftware.MonoX.DAL.EntityClasses;
+using MonoSoftware.MonoX.DAL.HelperClasses;
+using MonoSoftware.MonoX.Utilities;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoSoftware.MonoX.DAL.RelationClasses;
+
+namespace MonoSoftware.MonoX.Repositories
+{
+    /// <summary>
+    /// Repository for social network popular group-related functionality
+    /// </summary>
+    public class PopularGroupRepository : GroupRepository 
+    {
+
+        #region Fields
+        /// <summary>
+        /// MonoX popular groups list cache parameter.
+        /// </summary>
+        public const string CacheParamMonoXPopularGroupsList = "MonoXPopularGroupsList";
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected PopularGroupRepository()
+        { }
+
+        /// <summary>
+        /// Returns an instance of the repository class, wraps the constructor to allow for encapsulated instatiation, so the logic in he constructor can be changed without changing the client code.
+        /// Reference: http://www.netobjectives.com/ezines/ez0405NetObj_PerspectivesOfUseVsCreationInOODesign.pdf
+        /// </summary>
+        /// <returns>Instance of the repository.</returns>
+        public static new PopularGroupRepository GetInstance()
+        {
+            return new PopularGroupRepository();
+        } 
+        #endregion        
+
+        #region Methods
+        /// <summary>
+        /// Retrieves a list of groups matching the criteria specified via method parameters.
+        /// </summary>
+        /// <param name="category">Category name.</param>
+        /// <param name="categoryId">Category Id</param>
+        /// <param name="pageNumber">Page number.</param>
+        /// <param name="pageSize">Page size.</param>
+        /// <param name="recordCount">Record count.</param>
+        /// <returns>List of groups.</returns>
+        public List<SnGroupDTO> GetPopularGroups(string category, Guid categoryId, int pageNumber, int pageSize, out int recordCount)
+        {
+            RelationPredicateBucket filter = new RelationPredicateBucket();
+            
+            //introduced to filter out groups by languages and applications            
+            filter.Relations.Add(SnGroupEntity.Relations.SnGroupCategoryEntityUsingGroupCategoryId, JoinHint.Left);
+            //Note: MonoX supports the multi application environment so general filter for all DB access calls should contain the application id filter
+            filter.PredicateExpression.Add(SnGroupCategoryFields.ApplicationId == MembershipRepository.GetInstance().GetApplicationId());
+            //Note: MonoX in supports the multi language environment so general filter for all DB access calls should contain the language id filter
+            filter.PredicateExpression.Add(SnGroupCategoryFields.LanguageId == LocalizationUtility.GetCurrentLanguageId());
+
+            //Filter groups by category
+            if (categoryId != Guid.Empty)
+            {
+                filter.PredicateExpression.Add(SnGroupFields.GroupCategoryId == categoryId);
+            }
+            if (!String.IsNullOrEmpty(category))
+            {
+                filter.PredicateExpression.Add(SnGroupCategoryFields.Slug == category);
+            }
+
+            IPrefetchPath2 prefetch = new PrefetchPath2((int)EntityType.SnGroupEntity);
+            prefetch.Add(SnGroupEntity.PrefetchPathSnGroupCategory);
+            //Fetch a record from the members table only for the current user so his status could be read
+            Guid uid = SecurityUtility.GetUserId();
+            if (!Guid.Empty.Equals(uid))
+            {
+                PredicateExpression memberFilter = new PredicateExpression(SnGroupMemberFields.UserId == uid);
+                prefetch.Add(SnGroupEntity.PrefetchPathSnGroupMembers, 1, memberFilter);
+            }
+            
+            #region Popular groups sorter
+            const string memberCountField = "MemberCountField";
+            const string memberCountTableName = "MemberCountTable";
+
+            EntityFields2 memberFields = new EntityFields2(2);
+            memberFields.DefineField(SnGroupMemberFields.GroupId, 0);
+            memberFields.DefineField(SnGroupMemberFields.Id, 1, memberCountField, AggregateFunction.Count);
+            DerivedTableDefinition memberCountTable = new DerivedTableDefinition(memberFields, memberCountTableName, null, new GroupByCollection(memberFields[0]));
+
+            IDynamicRelation memberCountRelation = new DynamicRelation(memberCountTable, JoinHint.Right, MonoSoftware.MonoX.DAL.EntityType.SnGroupEntity, String.Empty, SnGroupMemberFields.GroupId.SetObjectAlias(memberCountTable.Alias) == SnGroupFields.Id);
+            filter.Relations.Add(memberCountRelation);
+
+            ISortExpression sorter = new SortExpression(new SortClause(new EntityField2(memberCountField, null).SetObjectAlias(memberCountTableName), null, SortOperator.Descending)); 
+            #endregion
+
+            EntityCollection<SnGroupEntity> groups = new EntityCollection<SnGroupEntity>();
+            //Fetch the group collection
+            FetchEntityCollection(groups, filter, 0, sorter, prefetch, pageNumber, pageSize);
+            //Fetch the group total count used by paging
+            recordCount = GetDbCount(groups, filter);
+            //Transfer group entities to the DTO
+            List<SnGroupDTO> toReturn = groups.Select(group => new SnGroupDTO(group)).ToList<SnGroupDTO>();
+            return toReturn;
+        }
+        
+        #endregion
+        
+
+
+
+    }
+
+}
---
+++ ProjectName/WebParts/PopularGroupsList.ascx.cs
@@ -0,0 +1,167 @@
+﻿using MonoSoftware.MonoX.Caching;
+using MonoSoftware.MonoX.Repositories;
+using MonoSoftware.MonoX.Resources;
+using MonoSoftware.MonoX.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace MonoSoftware.MonoX.ModuleGallery.SocialNetworking
+{
+    /// <summary>
+    /// Displays most popular groups ordered by their creation date, often used on dashboard pages. Inherits from the GroupListBasedPart.
+    /// </summary>
+    public partial class PopularGroupsList : GroupListBasePart
+    {
+        #region Properties
+        /// <summary>
+        /// Templated list view that is used for rendering the "repeatable" section of this control.
+        /// </summary>
+        [PersistenceMode(PersistenceMode.InnerProperty)]
+        public ListView TemplatedControl
+        {
+            get { return lvItems; }
+            set
+            {
+                pnlContainer.Controls.Remove(lvItems);
+                lvItems = value;
+                pnlContainer.Controls.AddAt(0, lvItems);
+            }
+        }
+
+        /// <summary>
+        /// Pager page size.
+        /// </summary>
+        [WebBrowsable(true), Personalizable(true)]
+        [WebDescription("Pager page size")]
+        [WebDisplayName("Pager page size")]
+        public int PageSize
+        {
+            get { return pager.PageSize; }
+            set { pager.PageSize = value; }
+        }
+
+        private bool _pagingEnabled = true;
+        /// <summary>
+        /// Gets or sets a value indicating whether paging is enabled.
+        /// </summary>
+        [WebBrowsable(true), Personalizable(true)]
+        [WebDescription("Paging enabled")]
+        [WebDisplayName("Paging enabled")]
+        public bool PagingEnabled
+        {
+            get { return _pagingEnabled; }
+            set { _pagingEnabled = value; }
+        }
+
+        private int _avatarSize = 80;
+        /// <summary>
+        /// Avatar size - 1 to 80 for gravatars.
+        /// </summary>
+        [WebBrowsable(true), Personalizable(true)]
+        [WebDescription("Avatar size")]
+        [WebDisplayName("Avatar size")]
+        public int AvatarSize
+        {
+            get { return _avatarSize; }
+            set { _avatarSize = value; }
+        }
+
+        private string _pagerQueryString = "PopularGroupsPageNo";
+        /// <summary>
+        /// Pager query string name.
+        /// </summary>
+        [WebBrowsable(true), Personalizable(true)]
+        [WebDescription("Pager query string name")]
+        [WebDisplayName("Pager query string name")]
+        public string PagerQueryString
+        {
+            get { return _pagerQueryString; }
+            set { _pagerQueryString = value; }
+        } 
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PopularGroupsList()
+        {
+            //Set the title that will be visible in the Module gallery so user can pick the module from the list
+            Title = "SN popular groups list";
+            //Define the template root folder for where MonoX can search for specified template
+            ControlSpecificTemplateSubPath = "SocialNetworkingTemplates";
+            //Define the template file name (without the extension)
+            Template = "PopularGroupsList";
+            IsTemplated = true;
+        } 
+        #endregion
+
+        #region Page Events
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            //Set the SEO pager query string so pager can read the proper page number form the Url
+            PagerUtility.SetCustomQueryStringName(this, pager, this.PagerQueryString);
+            this.TemplatedControl.ItemDataBound += new EventHandler<ListViewItemEventArgs>(lvItems_OnItemDataBound);
+            ltlNoData.Text = SocialNetworkingResources.Groups_NoDataWarning;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Optimize the binding process
+            if (this.Visible)
+                BindData();
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            //Hide the pager if there are no pages or paging is disabled
+            pager.Visible = (pager.PageCount > 1) && this.PagingEnabled;
+        } 
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Binds and formats group data for display in the main list view control.
+        /// </summary>
+        [WebPartApplyChanges]
+        public override void BindData()
+        {
+            //Optimize the DB access by using the MonoX cache mechanism (Note: To gain from this kind of optimization you need to set the CacheDuration for this web part)
+            MonoXCacheManager cacheManager = MonoXCacheManager.GetInstance(CacheRootKey, this.CacheDuration);
+            PopularGroupRepository repository = PopularGroupRepository.GetInstance();
+            int recordCount = cacheManager.Get<int>(PopularGroupRepository.CacheParamMonoXPopularGroupsList, "recordCount");
+            List<SnGroupDTO> groups = cacheManager.Get<List<SnGroupDTO>>(PopularGroupRepository.CacheParamMonoXPopularGroupsList, pager.CurrentPageIndex + 1, pager.PageSize);
+            if (groups == null)
+            {
+                //Fetch the popular groups
+                groups = repository.GetPopularGroups(String.Empty, Guid.Empty, pager.CurrentPageIndex + 1, pager.PageSize, out recordCount);
+                //Store the values to the cache (Note: If CacheDuration is set to "0" values are not saved to the cache)
+                cacheManager.Store(groups, PopularGroupRepository.CacheParamMonoXPopularGroupsList, pager.CurrentPageIndex + 1, pager.PageSize);
+                cacheManager.Store(recordCount, PopularGroupRepository.CacheParamMonoXPopularGroupsList, "recordCount");
+            }
+            //Perform the binding process that will automatically bind the control and pager
+            PagerUtility.BindPager(pager, BindData, lvItems, groups, recordCount);
+            ltlNoData.Visible = (recordCount == 0);
+        }
+
+        protected void lvItems_OnItemDataBound(object sender, ListViewItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListViewItemType.DataItem)
+            {
+                SnGroupDTO group = ((ListViewDataItem)e.Item).DataItem as SnGroupDTO;
+                //Parse template tags will extract all the needed information from SnGroupDTO and fill the tags collection with tags that are built-in 
+                //Note: To add any custom tags to web part template you need to override the ParseTemplateTags and add custom tags, then you need to place them in web part template file
+                Hashtable tags = ParseTemplateTags(group);
+                //Render templated item to the place holder
+                //Note: This built-in method will parse the provided html template and replace the tags with the values stored in the tags collection
+                RenderTemplatedPart(e.Item, CurrentTemplateHtml, tags);
+            }
+        } 
+        #endregion
+
+   }
+}
